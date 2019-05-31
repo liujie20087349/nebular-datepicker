65945d2394ab386b2797819c957f9234ca48be9e
刘杰 <garila@qq.com>
2019-05-31 15:33:35 +0800

    add content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,83 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  NbThemeModule,
+  NbLayoutModule,
+  NbCardModule,
+  NbDatepickerModule,
+  NbInputModule,
+  NbCalendarKitModule,
+  NbTooltipModule,
+  NbChatModule,
+  NbSelectModule,
+  NbRadioModule,
+  NbActionsModule,
+  NbMenuModule,
+  NbRouteTabsetModule,
+  NbSearchModule,
+  NbSidebarModule,
+  NbTabsetModule,
+  NbUserModule,
+  NbCheckboxModule,
+  NbPopoverModule,
+  NbContextMenuModule,
+  NbProgressBarModule,
+  NbCalendarModule,
+  NbCalendarRangeModule,
+  NbStepperModule,
+  NbButtonModule,
+  NbAccordionModule,
+  NbDialogModule,
+  NbWindowModule,
+  NbListModule,
+  NbToastrModule, NbAlertModule, NbSpinnerModule
+} from '@nebular/theme';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { APP_BASE_HREF, registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+import fr from '@angular/common/locales/fr';
+import en from '@angular/common/locales/en';
+
+
+registerLocaleData(zh);
+
+const nebular_imports = [
+  NbActionsModule,
+  NbCardModule,
+  NbLayoutModule,
+  NbMenuModule,
+  NbRouteTabsetModule,
+  NbSearchModule,
+  NbSidebarModule,
+  NbTabsetModule,
+  NbThemeModule,
+  NbUserModule,
+  NbCheckboxModule,
+  NbPopoverModule,
+  NbContextMenuModule,
+  NbProgressBarModule,
+  NbCalendarModule,
+  NbCalendarRangeModule,
+  NbStepperModule,
+  NbButtonModule,
+  NbInputModule,
+  NbAccordionModule,
+  NbDatepickerModule,
+  NbDialogModule,
+  NbWindowModule,
+  NbListModule,
+  NbToastrModule,
+  NbAlertModule,
+  NbSpinnerModule,
+  NbRadioModule,
+  NbSelectModule,
+  NbChatModule,
+  NbTooltipModule,
+  NbCalendarKitModule,
+];
 
 @NgModule({
   declarations: [
@@ -10,9 +85,25 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ...nebular_imports,
+    NbThemeModule.forRoot({ name: 'cosmic' }),
+
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_BASE_HREF, useValue: '/'
+    },
+    ...NbDatepickerModule.forRoot().providers,
+    {
+      provide: LOCALE_ID, useValue: 'zh-cn'
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+}
